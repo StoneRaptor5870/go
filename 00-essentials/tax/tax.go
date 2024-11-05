c690ce1a7f102cf33f2e1fa71df73d319e9f5dcc
Nischay <nischay.dtu@gmail.com>
2024-11-06 05:08:40 +0530

    file

--- 00-essentials/tax/tax.go
+++ 00-essentials/tax/tax.go
@@ -1,22 +1,67 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 func main() {
-	revenue := getUserInput("Revenue: ")
-	expenses := getUserInput("Expenses: ")
-	rate := getUserInput("Rate: ")
+	revenue, err := getUserInput("Revenue: ")
 
-	ebt := revenue - expenses
-	profit := ebt * (1 - rate/100)
-	ratio := ebt / profit
+	if err != nil {
+		fmt.Println(err)
+		return
+		// panic(err)
+	}
 
-	fmt.Println(ebt, profit, ratio)
+	expenses, err := getUserInput("Expenses: ")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	taxRate, err := getUserInput("Tax Rate: ")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// if err1 != nil || err2 != nil || err3 != nil {
+	// 	fmt.Println(err1)
+	// 	return
+	// }
+
+	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
+
+	fmt.Printf("%.1f\n", ebt)
+	fmt.Printf("%.1f\n", profit)
+	fmt.Printf("%.3f\n", ratio)
+	storeResults(ebt, profit, ratio)
 }
 
-func getUserInput(infoText string) float64 {
+func storeResults(ebt, profit, ratio float64) {
+	results := fmt.Sprintf("EBT: %.1f\nProfit: %.1f\nRatio: %.3f\n", ebt, profit, ratio)
+	os.WriteFile("results.txt", []byte(results), 0644)
+}
+
+func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64) {
+	ebt := revenue - expenses
+	profit := ebt * (1 - taxRate/100)
+	ratio := ebt / profit
+	return ebt, profit, ratio
+}
+
+func getUserInput(infoText string) (float64, error) {
 	var userInput float64
 	fmt.Print(infoText)
 	fmt.Scan(&userInput)
-	return userInput
+
+	if userInput <= 0 {
+		return 0, errors.New("value must be a positive number")
+	}
+
+	return userInput, nil
 }
