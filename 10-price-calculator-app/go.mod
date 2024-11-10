6d0f973413876e6b36fb55ae223ced101b52c492
Nischay <nischay.dtu@gmail.com>
2024-11-10 23:45:21 +0530

    price calculator

---
+++ 10-price-calculator-app/go.mod
@@ -0,0 +1,3 @@
+module price_calculator
+
+go 1.22.2
---
+++ 10-price-calculator-app/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"price_calculator/filemanager"
+	"price_calculator/prices"
+)
+
+func main() {
+	taxRates := []float64{0, 0.07, 0.1, 0.15}
+
+	for _, taxRate := range taxRates {
+		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+		// cmdm := cmdmanager.New()
+		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
+		err := priceJob.Process()
+
+		if err != nil {
+			fmt.Println("could not process job", err)
+		}
+	}
+}
