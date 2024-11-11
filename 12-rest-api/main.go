52621ef61802c35fbd491edb64f973b19bc2483f
Nischay <nischay.dtu@gmail.com>
2024-11-11 17:12:36 +0530

    CRUD operations, database, routes and model

---
+++ 12-rest-api/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"rest_api/db"
+	"rest_api/routes"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	db.InitDB()
+	server := gin.Default()
+	routes.RegisterRoutes(server)
+	server.Run(":8080")
+}
