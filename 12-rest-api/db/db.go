1bb66c1886a775ff0cf7ea1670327722ecbe6222
Nischay <nischay.dtu@gmail.com>
2024-11-14 22:57:28 +0530

    postgres & godotenv

--- 12-rest-api/db/db.go
+++ 12-rest-api/db/db.go
@@ -2,15 +2,17 @@ package db
 
 import (
 	"database/sql"
+	"os"
 
-	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/lib/pq"
 )
 
 var DB *sql.DB
 
 func InitDB() {
 	var err error
-	DB, err = sql.Open("sqlite3", "api.db")
+	connectionString := os.Getenv("DB")
+	DB, err = sql.Open("postgres", connectionString)
 
 	if err != nil {
 		panic("Could not connect to database.")
@@ -25,7 +27,7 @@ func InitDB() {
 func createTables() {
 	createUsersTable := `
 	CREATE TABLE IF NOT EXISTS users (
-	  id INTEGER PRIMARY KEY AUTOINCREMENT,
+	  id SERIAL PRIMARY KEY,
 	  email TEXT NOT NULL UNIQUE,
 	  password TEXT NOT NULL
 	)
@@ -39,11 +41,11 @@ func createTables() {
 
 	createEventsTable := `
 	CREATE TABLE IF NOT EXISTS events (
-	  id INTEGER PRIMARY KEY AUTOINCREMENT,
+	  id SERIAL PRIMARY KEY,
 	  name TEXT NOT NULL,
 	  description TEXT NOT NULL,
 	  location TEXT NOT NULL,
-	  dateTime DATETIME NOT NULL,
+	  dateTime TIMESTAMP NOT NULL,
 	  user_id INTEGER,
 	  FOREIGN KEY(user_id) REFERENCES users(id)
 	)
@@ -59,8 +61,9 @@ func createTables() {
 	CREATE TABLE IF NOT EXISTS registrations (
 	  event_id INTEGER,
 	  user_id INTEGER,
-	  FOREIGN KEY(event_id) REFERENCES events(id),
-	  FOREIGN KEY(user_id) REFERENCES users(id)
+	  FOREIGN KEY (event_id) REFERENCES events(id),
+	  FOREIGN KEY (user_id) REFERENCES users(id),
+	  PRIMARY KEY (event_id, user_id)
 	)
 	`
 
--- 12-rest-api/main.go
+++ 12-rest-api/main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
 	"rest_api/db"
 	"rest_api/routes"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	db.InitDB()
 	server := gin.Default()
 	routes.RegisterRoutes(server)
-	server.Run(":8080")
+	port := os.Getenv("PORT")
+	server.Run(fmt.Sprintf(`:%s`, port))
 }
